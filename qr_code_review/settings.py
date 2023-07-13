9a8ec2ac57cbb131ff5b2a0f01254ee5da05f9d5
brianchege2k <chegebrian84@gmail.com>
2023-07-13 16:08:43 +0300

    New updates for Heroku deployment

--- qr_code_review/settings.py
+++ qr_code_review/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t$ch(i3di3f8%y=5cs*w&gl(v6%1(#m&1cmi!w^7@+0uvk9v0&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
