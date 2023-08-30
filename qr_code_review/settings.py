f00c0082c1ee6bd1bbf83f77aab936e242b0565d
brianchege2k <chegebrian84@gmail.com>
2023-08-30 20:47:43 +0300

    more changes

--- qr_code_review/settings.py
+++ qr_code_review/settings.py
@@ -38,13 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'reviews',
     'whitenoise.runserver_nostatic',
-    'corsheaders',
+    
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -138,5 +137,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
-CORS_ALLOW_ALL_ORIGINS = True
+
 
